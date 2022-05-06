5d710e80c5279db056eb3ce67d56fcddaa47acb6
= <=>
2022-05-06 15:35:46 +0200

    projet chartistjs

---
+++ bar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>teste bar Chartist.js</title>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+    <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+</head>
+<body>
+
+
+<script>
+    var chart = new Chartist.Line('.ct-chart', {
+        labels: [1, 2, 3],
+        series: [
+            [
+                {meta: 'description', value: 1 },
+                {meta: 'description', value: 5},
+                {meta: 'description', value: 3}
+            ],
+            [
+                {meta: 'other description', value: 2},
+                {meta: 'other description', value: 4},
+                {meta: 'other description', value: 2}
+            ]
+        ]
+    }, {
+        low: 0,
+        high: 8,
+        fullWidth: true,
+        plugins: [
+            Chartist.plugins.tooltip()
+        ]
+    });
+</script>
+</body>
+</html>
